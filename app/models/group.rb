2f08bf786121c47b8c7b091fb5808f2c19af49e0
reyon08 <sorairo.benimairoi@gmail.com>
2020-01-16 21:50:54 +0900

    messages-function
    メッセージの送信機能を記述しました

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,16 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message #ビューに条件分岐をすると、読みづらくなる。こういう時はモデルにインスタンスメソッドを定義しましょう
+    if (last_message = messages.last).present?
+      if last_message.text?
+        last_message.text
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません'
+    end
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to: user
-  belongs_to: group
-  validates :content, presence: true, unless: :image?
+  belongs_to :group
+  belongs_to :user
+  validates :text, presence: true, unless: :image?
+  mount_uploader :image, ImageUploader # app/uploaders/image_uploader.rb で初登場
 end
