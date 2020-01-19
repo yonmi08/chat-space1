71a04f7e613566df645a5da7dbc195ef26905f4e
reyon08 <sorairo.benimairoi@gmail.com>
2020-01-19 14:30:10 +0900

    about asynchronous
    コメント送信の非同期化に関する記述をしました

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace1
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
